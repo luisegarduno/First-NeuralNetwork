405fe622c74dd3b2430d34acda1850263797b944
luisegarduno <luis.e.garduno@gmail.com>
2020-06-17 03:34:11 -0500

    Part 4 includes examples of batches, dot product, and eventually a forward pass between layers. Will go back and add more comments next time

---
+++ Part 4/py4_1.py
@@ -0,0 +1,30 @@
+import numpy as np
+												
+inputs   = [ [  1.0,     2.0,   3.0,   2.5], 	# inputs shape: (3,4)
+             [  2.0,     5.0,  -1.0,   2.0],
+             [ -1.5,     2.7,   3.3,  -0.8] ]
+
+             									
+weights  = [ [  0.2,     0.8,  -0.5,   1.0],	# weights shape: (3,4)
+			 [  0.5,   -0.91,  0.26,  -0.5],	
+			 [-0.26,   -0.27,  0.17,  0.87] ]	
+
+biases = [2, 3, 0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+
+
+
+
+
+weights2  = [ [  0.1,  -0.14,  0.50],	# weights shape: (3,4)
+			  [ -0.5,   0.12, -0.33],	
+			  [-0.44,  -0.73,  0.13] ]	
+
+biases2 = [-1, 2, -0.5] 						
+
+
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
