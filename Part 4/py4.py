b6577aa3bf5294ef8758a1bed3030e27061ea88c
luisegarduno <luis.e.garduno@gmail.com>
2024-10-11 23:15:45 -0400

    Minor formatting changes + added jupyter nb

--- Part 4/py4.py
+++ Part 4/py4.py
@@ -1,24 +1,24 @@
 import numpy as np
-												
+
 inputs   = [ [  1.0,     2.0,   3.0,   2.5], 	# inputs shape: (3,4)
              [  2.0,     5.0,  -1.0,   2.0],
              [ -1.5,     2.7,   3.3,  -0.8] ]
 
-             									
-weights  = [ [  0.2,     0.8,  -0.5,   1.0],	# weights shape: (3,4)
-			 [  0.5,   -0.91,  0.26,  -0.5],	
-			 [-0.26,   -0.27,  0.17,  0.87] ]	
+
+weights  = [ [  0.2,     0.8,  -0.5,   1.0],    # weights shape: (3,4)
+             [  0.5,   -0.91,  0.26,  -0.5],
+             [-0.26,   -0.27,  0.17,  0.87] ]
 
 biases = [2, 3, 0.5] 						
 
 
 output = np.dot(inputs, np.array(weights).T) + biases
 # We recieve an error after trying to do this because this is what we are trying to do:
-#			weights								inputs
-#	 [  1,1	 1,2  1,3  1,4				[  1,1	1,2  1,3  1,4
-#	    2,1  2,2  2,3  2,4		 *	       2,1  2,2  2,3  2,4
-#		3,1  3,2  3,2  3,4 ]			   3,1  3,2  3,2  3,4 ]
-#       
+#               weights					inputs
+#        [  1,1	 1,2  1,3  1,4                  [  1,1	1,2  1,3  1,4
+#           2,1  2,2  2,3  2,4          *          2,1  2,2  2,3  2,4
+#           3,1  3,2  3,2  3,4 ]                   3,1  3,2  3,2  3,4 ]
+#
 #   (1,1) * (1,1)  +
 #   (1,2) * (2,1)  +
 #   (1,3) * (3,1)  +
--- Part 4/py4_2.py
+++ Part 4/py4_2.py
@@ -1,29 +1,29 @@
 import numpy as np
 
 np.random.seed(0)
-												
+
 X	= [ [  1.0,     2.0,   3.0,   2.5], 	# X shape: (3,4)
-        [  2.0,     5.0,  -1.0,   2.0],
-        [ -1.5,     2.7,   3.3,  -0.8] ]
+            [  2.0,     5.0,  -1.0,   2.0],
+            [ -1.5,     2.7,   3.3,  -0.8] ]
 
 
 class Layer_Dense:
-	def __init__(self, n_inputs, n_neurons):
-		# We want numpy to create a weights that will be the size of
-		# number of inputs times the number of neurons we want
+    def __init__(self, n_inputs, n_neurons):
+        # We want numpy to create a weights that will be the size of
+        # number of inputs times the number of neurons we want
 
-		# The parameters of randn is the shape,
-		# in this case, randn(number of columns, number of rows) 
-		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        # The parameters of randn is the shape
+        # in this case, randn(number of columns, number of rows)
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
 
-		# first parameter in np.zeros is the shape
-		# Remember, each neuron has a unique bias, so that is how many
-		# biases we will initially generate
-		self.biases = np.zeros((1, n_neurons))
-		# what randn does is just a Gaussian Distribution around 0
+        # first parameter in np.zeros is the shape
+        # Remember, each neuron has a unique bias, so that is how many
+        # biases we will initially generate
+        self.biases = np.zeros((1, n_neurons))
+        # what randn does is just a Gaussian Distribution around 0
 
-	def forward(self, inputs):
-		self.output = np.dot(inputs, self.weights) + self.biases
+        def forward(self, inputs):
+            self.output = np.dot(inputs, self.weights) + self.biases
 
 # This produces values greater than 1
 # print(np.random.randn(4,3))
@@ -33,7 +33,7 @@ class Layer_Dense:
 
 # First parameter, how many features in each sample(X), in this case we have 4
 # Second parameter,(number of neurons) anything you want
-layer1 = Layer_Dense(4,5)
+layer1 = Layer_Dense(4, 5)
 
 layer1.forward(X)
 #print(layer1.output)
@@ -41,7 +41,7 @@ layer1.forward(X)
 
 # Only requirement for layer2, is that the first parameter has to be the
 # output of layer1, in this situation it is 5
-layer2 = Layer_Dense(5,2)
+layer2 = Layer_Dense(5, 2)
 layer2.forward(layer1.output)
 # It output a matrix of size 3, since that was the batch size in the X (inputs),
 # with 2 columns since that the number of neurons stated for this layer
