8668bfec4c36daaec5cf0d639dab84d7d70f3aea
luisegarduno <luis.e.garduno@gmail.com>
2024-10-11 23:30:49 -0400

    Minor formatting fixes

--- Part 5/py5.py
+++ Part 5/py5.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 np.random.seed(0)
-												
-X	= [ [  1.0,     2.0,   3.0,   2.5], 	# X shape: (3,4)
-        [  2.0,     5.0,  -1.0,   2.0],
-        [ -1.5,     2.7,   3.3,  -0.8] ]
+
+X	= [ [  1.0,		2.0,   3.0,	  2.5], 	# X shape: (3,4)
+		[  2.0,		5.0,  -1.0,	  2.0],
+		[ -1.5,		2.7,   3.3,  -0.8] ]
 
 '''
 inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
@@ -20,7 +20,6 @@ for i in inputs:
 
 # Or
 
-
 # append the maximum of 0 or i
 for i in inputs:
 	output.append(max(0,i))
@@ -31,11 +30,11 @@ print(output)
 
 class Layer_Dense:
 	def __init__(self, n_inputs, n_neurons):
-		# We want numpy to create a weights that will be the size of
+		# We want numpy to create a weights that will be the size o
 		# number of inputs times the number of neurons we want
 
 		# The parameters of randn is the shape,
-		# in this case, randn(number of columns, number of rows) 
+		# in this case, randn(number of columns, number of rows)
 		self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
 
 		# first parameter in np.zeros is the shape
@@ -52,6 +51,7 @@ class Layer_Dense:
 class Activation_ReLU:
 	def forward(self,inputs):
 		self.outputs = np.maximum(0, inputs)
+
 
 layer1 = Layer_Dense(4,5)
 layer1.forward(X)
--- Part 5/py5_1.py
+++ Part 5/py5_1.py
@@ -3,10 +3,10 @@ import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
-												
-X	= [ [  1.0,     2.0,   3.0,   2.5], 	# X shape: (3,4)
-        [  2.0,     5.0,  -1.0,   2.0],
-        [ -1.5,     2.7,   3.3,  -0.8] ]
+
+X	= [ [  1.0,     2.0,   3.0,   2.5],		# X shape: (3,4)
+		[  2.0,     5.0,  -1.0,   2.0],
+		[ -1.5,     2.7,   3.3,  -0.8] ]
 
 
 X, y = spiral_data(100, 3)
@@ -24,12 +24,12 @@ class Activation_ReLU:
 	def forward(self,inputs):
 		self.output = np.maximum(0, inputs)
 
+
 layer1 = Layer_Dense(2,5)
 activation1 = Activation_ReLU()
 
 layer1.forward(X)
-#print(layer1.output)
-
+# print(layer1.output)
 
 activation1.forward(layer1.output)
 print(activation1.output)
